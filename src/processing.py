22563170d32ddce3516d4b3c99b5276c1dfd08cf
VladislavRyabokon <rbibokon@yandex.ru>
2025-02-15 20:49:09 +0700

    add function in processing.py

--- src/processing.py
+++ src/processing.py
@@ -1,12 +1,18 @@
-def filter_by_state(state='EXECUTED'):
+def filter_by_state(list_transaction, state='EXECUTED'):
     """Функция принимает словари, а возвращает новый список словарей,
      который содержит только словари с ключом"""
 
-    pass
+    buffer_list = []
+    for i in list_transaction:
+        if i['state'] == state:
+            buffer_list.append(i)
+    return buffer_list
 
 
-def sort_by_date(date=False):
+
+def sort_by_date(list_transaction, date=True):
     """Функция принимает список словарей,
     а возвращает список отсортированный по дате"""
 
-    pass
+    return sorted(list_transaction, key=lambda x: x['date'], reverse=date)
+
