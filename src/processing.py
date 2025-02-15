3df01fce0b50d85c983d55de000d44026b5eb21a
VladislavRyabokon <rbibokon@yandex.ru>
2025-02-16 00:40:13 +0700

    fixes processing.py

--- src/processing.py
+++ src/processing.py
@@ -1,7 +1,6 @@
 def filter_by_state(list_transaction: list[dict[str, str]], state: str = "EXECUTED"):
     """Функция принимает словари, а возвращает новый список словарей,
     который содержит только словари с ключом"""
-
     buffer_list = []
     for i in list_transaction:
         if i["state"] == state:
@@ -12,5 +11,4 @@ def filter_by_state(list_transaction: list[dict[str, str]], state: str = "EXECUT
 def sort_by_date(list_transaction: list[dict[str, str]], date: bool = True):
     """Функция принимает список словарей,
     а возвращает список отсортированный по дате"""
-
     return sorted(list_transaction, key=lambda x: x["date"], reverse=date)
