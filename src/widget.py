3242737edd948a82eaf1805f23f3859c7274e6b4
VladislavRyabokon <rbibokon@yandex.ru>
2025-02-16 21:02:47 +0700

    reworked function date in widget

--- src/widget.py
+++ src/widget.py
@@ -1,8 +1,9 @@
 from src import masks
+from datetime import datetime
 
 
 def mask_account_card(card: str) -> str:
-    '''Функция принимающая тип карты или счета и возвращающая скрытую часть'''
+    """Функция принимающая тип карты или счета и возвращающая скрытую часть"""
     number = ""
     text = ""
     for i in card:
@@ -17,16 +18,7 @@ def mask_account_card(card: str) -> str:
 
 
 def get_date(date: str) -> str:
-    '''Функция принимающая строку в определенном формате и возвращает дд.мм.гггг'''
-    date_buffer = ""
-    correct_date = ""
-    for i in date:
-        if i != "T" and i != "-":
-            date_buffer += i
-        elif i == "-":
-            continue
-        else:
-            break
-    correct_date = (date_buffer[-1] + date_buffer[-2] + "." + date_buffer[-4] + date_buffer[-3] + "."
-                    + date_buffer[0] + date_buffer[1] + date_buffer[2] + date_buffer[3])
-    return correct_date
+    """Функция принимающая строку в определенном формате и возвращает дд.мм.гггг"""
+    date_obj = datetime.strptime(date, "%Y-%m-%dT%H:%M:%S.%f")
+    formatted_date = date_obj.strftime("%d.%m.%Y")
+    return formatted_date
