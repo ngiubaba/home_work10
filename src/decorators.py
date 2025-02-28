40a47960361469f66c99667023e10f9c41bcea91
VladislavRyabokon <rbibokon@yandex.ru>
2025-02-28 15:50:15 +0700

    add decorators.py add decorator

---
+++ src/decorators.py
@@ -0,0 +1,26 @@
+import  datetime
+
+def log(filename=None):
+    def log_function(func):
+        def wrapper(*args, **kwargs):
+            start_time = datetime.datetime.now().strftime("%d.%m.%y %H:%M:%S")
+            start_message = f"{start_time} Функция {func.__name__} начала работу"
+            try:
+                result = func(*args, **kwargs)
+                log_message = f"{func.__name__} ok"
+                buffer_result = result
+
+            except Exception as e:
+                error_message = str(e)
+                log_message = (f"{start_time} Функция {func.__name__} error: {error_message}."
+                               f" Inputs: ({args}, {kwargs})")
+                buffer_result = None
+
+            if filename:
+                with open(filename, 'a') as file:
+                    file.write(log_message + "\n")
+            else:
+                print(log_message)
+            return buffer_result
+        return wrapper
+    return log_function
