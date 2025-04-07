fda83f00caf1e084e01333421a8ceffef4a08990
VladislavRyabokon <rbibokon@yandex.ru>
2025-04-07 22:27:49 +0700

    add function find_by_description

--- src/processing.py
+++ src/processing.py
@@ -1,3 +1,6 @@
+import re
+
+
 def filter_by_state(list_transaction: list[dict[str, str]], state: str = "EXECUTED") -> list:
     """Функция принимает словари, а возвращает новый список словарей,
     который содержит только словари с ключом"""
@@ -20,3 +23,11 @@ def sort_by_date(list_transaction: list[dict[str, str]], date: bool = True) -> l
         raise LengthListError("Введено пустое значение")
     else:
         return sorted(list_transaction, key=lambda x: x["date"], reverse=date)
+
+
+def find_by_description(list_transaction: list, find_word: str) -> list:
+    """Функция принимает список словарей и строку поиска,
+    возвращает списки в которых есть эта строка"""
+    r = re.compile(find_word, re.IGNORECASE)
+    filtered_transactions = filter(lambda x: r.search(x["description"]), list_transaction)
+    return list(filtered_transactions)
